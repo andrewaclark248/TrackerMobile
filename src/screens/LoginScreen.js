a50dd5b298f81c980e3da3150e4f01611c0eff6e
andrew <andrewacalrk248@gmail.com>
2022-01-30 10:36:22 -0500

    create initial routing

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const LoginScreen = () => {
+
+    return <Text>This is the login screen</Text>
+}
+
+
+export default LoginScreen;
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const WelcomeScreen = () => {
+
+    return <Text>This is the components screen</Text>
+}
+
+
+export default WelcomeScreen;
