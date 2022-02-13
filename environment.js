f3548b30b4d7a3d5120be648eb8c5e2fd1f47fa1
andrew <andrewacalrk248@gmail.com>
2022-02-13 12:42:23 -0500

    add new env var

--- environment.js
+++ environment.js
@@ -1,37 +1,22 @@
 import Constants from "expo-constants";
 import { Platform } from "react-native";
+import * as Updates from 'expo-updates';
+
 
 const localhost =
  Platform.OS === "ios" ? "localhost:8080" : "10.0.2.2:8080";
 
-const ENV = {
- dev: {
-   apiUrl: localhost,
-   amplitudeApiKey: null,
- },
- staging: {
-   apiUrl: "[your.staging.api.here]",
-   amplitudeApiKey: "[Enter your key here]",
-   // Add other keys you want here
- },
- prod: {
-   apiUrl: "[your.production.api.here]",
-   amplitudeApiKey: "[Enter your key here]",
-   // Add other keys you want here
- }
-};
+ function getEnvironment() {
+  if (Updates.releaseChannel.startsWith('prod')) {
+    // matches prod-v1, prod-v2, prod-v3
+    return { envName: 'PRODUCTION', apiUrl: 'productionURL.com'}; // prod env settings
+  } else if (Updates.releaseChannel.startsWith('staging')) {
+    // matches staging-v1, staging-v2
+    return { envName: 'STAGING', apiUrl: 'stagingURL.com' }; // stage env settings
+  } else {
+    // assume any other release channel is development
+    return { envName: 'DEVELOPMENT', apiUrl: 'localhost:3000'}; // dev env settings
+  }
+}
 
-const getEnvVars = (env = Constants.manifest.releaseChannel) => {
- // What is __DEV__ ?
- // This variable is set to true when react-native is running in Dev mode.
- // __DEV__ is true when run locally, but false when published.
- if (__DEV__) {
-   return ENV.dev;
- } else if (env === 'staging') {
-   return ENV.staging;
- } else if (env === 'prod') {
-   return ENV.prod;
- }
-};
-
-export default getEnvVars;
+export default getEnvironment;
